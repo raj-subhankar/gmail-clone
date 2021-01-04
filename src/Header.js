ea4cfe992cde16f8c6b85d62316affd7a01aedb8
Subhankar Raj <subhankar.rj@gmail.com>
2021-01-04 23:00:28 +0530

    Implement Header

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from "react"
+import "./Header.css"
+import MenuIcon from "@material-ui/icons/Menu"
+import SearchIcon from "@material-ui/icons/Search"
+import { Avatar, IconButton } from "@material-ui/core"
+import AppsIcon from "@material-ui/icons/Apps"
+import NotificationsIcon from "@material-ui/icons/Notifications"
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown"
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="header__left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r2.png"/>
+                
+            </div>
+
+            <div className="header__middle">
+                <SearchIcon/>
+                <input placeholder="Search mail" type="text"/>
+                <ArrowDropDownIcon className="header__inputCaret"/>
+            </div>
+
+            <div className="header__right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
